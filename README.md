2b5bdcb7a0ac49e59ee3f9a44f540e30bde1b52e
gioppoluca <gioppoluca@libero.it>
2014-05-20 11:43:24 +0200

    Fixed documentation

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ wso2api-php-lib
 
 PHP library to call WSO2 API Manager to create and publish API.
 This wraps the cURL calls available from API manager with simple calls.
-
+This works with the API Manager 1.6.0, but should work also with the 1.5.0.
+The API mapped are:
+- publisher/login
+- publisher/item-add/addAPI
+- publisher/life-cycles/updateStatus
+- publisher/listing/getAllAPIs
+- publisher/listing/getAPI
+- registry call to get the Swagger file
 
 
 Here is a sample of usage
@@ -37,3 +44,5 @@ $create_api_resources = array (array('resourceMethod'=>'GET',
 							         'uriTemplate'=>'/*'));
 $ret = $wso2api->create_api($create_api_post, $create_api_resources, $autopublish = true);
 ```
+
+To have a more complete example look at the test.php file
--- test.php
+++ test.php
@@ -1,5 +1,15 @@
 <?php
-include('wso2api.class.php');
+/**
+ * Testing file for checking the functioning of the wso2api class
+ * 
+ * @author Luca Gioppo
+ * @created 28/11/2013
+ * @copyright 2013 Luca Gioppo
+ * @version 0.1
+ * @license     https://github.com/gioppoluca/wso2api-php-lib/blob/master/LICENSE
+ * @package wso2api
+ */
+include ('wso2api.class.php');
 
 echo '<pre>';
 
@@ -7,86 +17,87 @@ echo 'Alcune informazioni di debug:';
 
 print "</pre>";
 
+$wso2api = new Wso2API ( "http://10.118.8.67:9763", $user = 'admin', $password = 'admin', $debug = true );
 
-$wso2api = new Wso2API("http://10.118.8.67:9763",$user = 'admin', $password = 'admin', $debug = true);
-
-
-switch ($_GET["test"]){
-	case "create":
-
-		/////////// create API test
-		$create_api_post = array('name'=>'WikipediaAPI',
-								 'visibility'=>'public',
-								 'version'=>'1.0.0',
-								 'description'=>'If you want to monitor a MediaWiki installation',
-								 'endpointType'=>'nonsecured',
-								 'http'=>true,
-								 'https'=>false,
-								 'endpoint'=>'http://en.wikipedia.org/w/api.php',
-								 'wsdl'=>'',
-								 'tags'=>'wikipedia,mediawiki',
-								 'tier'=>'Silver',
-								 'thumbUrl'=>'https://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png',
-								 'bizOwner'=>'Luca Gioppo',
-								 'provider'=>'admin',
-								 'context'=>'/wikipedia',
-								 'tiersCollection'=>'Gold',
-								);
-							
-		$create_api_resources = array (array('resourceMethod'=>'GET',
-											 'resourceMethodAuthType'=>'Application',
-											 'resourceMethodThrottlingTier'=>'Unlimited',
-											 'uriTemplate'=>'/*'));
-		$ret = $wso2api->create_api($create_api_post, $create_api_resources, $autopublish = true);
-
-		echo print_r($wso2api->error_message,true);
+switch ($_GET ["test"]) {
+	case "create" :	
+		// ///////// create API test
+		$create_api_post = array (
+				'name' => 'WikipediaAPI',
+				'visibility' => 'public',
+				'version' => '1.0.0',
+				'description' => 'If you want to monitor a MediaWiki installation',
+				'endpointType' => 'nonsecured',
+				'http' => true,
+				'https' => false,
+				'endpoint' => 'http://en.wikipedia.org/w/api.php',
+				'wsdl' => '',
+				'tags' => 'wikipedia,mediawiki',
+				'tier' => 'Silver',
+				'thumbUrl' => 'https://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png',
+				'bizOwner' => 'Luca Gioppo',
+				'provider' => 'admin',
+				'context' => '/wikipedia',
+				'tiersCollection' => 'Gold' 
+		);
+		
+		$create_api_resources = array (
+				array (
+						'resourceMethod' => 'GET',
+						'resourceMethodAuthType' => 'Application',
+						'resourceMethodThrottlingTier' => 'Unlimited',
+						'uriTemplate' => '/*' 
+				) 
+		);
+		$ret = $wso2api->create_api ( $create_api_post, $create_api_resources, $autopublish = true );
+		
+		echo print_r ( $wso2api->error_message, true );
 		break;
-	case "listApi":
-		/////// List API
-		$ret = $wso2api->get_all_api_list();
-		//echo print_r($wso2api->error_message,true);
+	case "listApi" :
+		// ///// List API
+		$ret = $wso2api->get_all_api_list ();
+		// echo print_r($wso2api->error_message,true);
 		echo '<pre>';
-
+		
 		echo 'result of call:';
-		if ($ret){
-		echo 'Response: ' . print_r($wso2api->response, TRUE);
-		}else{
-		echo 'Error message' . $wso2api->error_message;
-		echo 'Error message code' . $wso2api->error_code;
+		if ($ret) {
+			echo 'Response: ' . print_r ( $wso2api->response, TRUE );
+		} else {
+			echo 'Error message' . $wso2api->error_message;
+			echo 'Error message code' . $wso2api->error_code;
 		}
 		print "</pre>";
-
+		
 		break;
-	case "getSwagger":
-		/////// List API
+	case "getSwagger" :
+		// ///// List API
 		$apiname = 'citydynamics';
 		$apiversion = '1.0.0';
-		$ret = $wso2api->get_api_swagger($apiname, $apiversion);
+		$ret = $wso2api->get_api_swagger ( $apiname, $apiversion );
 		echo 'result of call:';
-		if ($ret){
-			echo 'Response: ' . print_r($wso2api->response, TRUE);
-		}else{
+		if ($ret) {
+			echo 'Response: ' . print_r ( $wso2api->response, TRUE );
+		} else {
 			echo 'Error message' . $wso2api->error_message;
 			echo 'Error message code' . $wso2api->error_code;
 		}
 		print "</pre>";
 		break;
-	case "getAPI":
-		/////// List API
+	case "getAPI" :
+		// ///// List API
 		$apiname = 'citydynamics';
 		$apiversion = '1.0.0';
 		$apiprovider = 'admin';
-		$ret = $wso2api->get_api_info($apiprovider, $apiname, $apiversion);
+		$ret = $wso2api->get_api_info ( $apiprovider, $apiname, $apiversion );
 		echo 'result of call:';
-		if ($ret){
-			echo 'Response: ' . print_r($wso2api->response, TRUE);
-		}else{
+		if ($ret) {
+			echo 'Response: ' . print_r ( $wso2api->response, TRUE );
+		} else {
 			echo 'Error message' . $wso2api->error_message;
 			echo 'Error message code' . $wso2api->error_code;
 		}
 		print "</pre>";
 		break;
-
 }
 
 ?>
